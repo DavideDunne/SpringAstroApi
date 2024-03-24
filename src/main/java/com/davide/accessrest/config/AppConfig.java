64b4c4e17d42830dcfff4048e685998a095e72dd
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-03-24 14:04:09 -0600

    webclient url obtained from application.properties

--- src/main/java/com/davide/accessrest/config/AppConfig.java
+++ src/main/java/com/davide/accessrest/config/AppConfig.java
@@ -5,12 +5,17 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
     @Bean
     public RestTemplate astroRestTemplate(RestTemplateBuilder builder, @Value("${astro.baseUrl}") String baseUrl) {
         return builder.rootUri(baseUrl).build();
+    }
+    @Bean
+    public WebClient astroWebClient(@Value("${astro.baseUrl}") String baseUrl) {
+        return WebClient.create(baseUrl);
     }
     @Bean
     public RestTemplate anotherRestTemplate(RestTemplateBuilder builder) {
--- src/main/java/com/davide/accessrest/services/AstroService.java
+++ src/main/java/com/davide/accessrest/services/AstroService.java
@@ -3,6 +3,7 @@ package com.davide.accessrest.services;
 import com.davide.accessrest.json.AstroResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +13,9 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class AstroService {
     private final RestTemplate template;
     private final WebClient client;
-    public AstroService(@Qualifier("astroRestTemplate") RestTemplate template) {
+    public AstroService(@Qualifier("astroRestTemplate") RestTemplate template,@Qualifier("astroWebClient") WebClient client) {
         this.template = template;
-        this.client = WebClient.create("http://api.open-notify.org");
+        this.client = client;
     }
     public String getPeopleInSpace() {
         return this.template.getForObject("/astros.json", String.class);
