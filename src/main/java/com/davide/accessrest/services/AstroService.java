a8867570578d5eb1d5785e7cf7b321ed33027054
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2024-03-24 14:06:21 -0600

    delete unused imports

--- src/main/java/com/davide/accessrest/services/AstroService.java
+++ src/main/java/com/davide/accessrest/services/AstroService.java
@@ -1,10 +1,7 @@
 package com.davide.accessrest.services;
 
 import com.davide.accessrest.json.AstroResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
